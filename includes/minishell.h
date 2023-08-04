696941330a680a80950361bc647ed63752663b40
Eric Cabanas <ecabanas@car8s5.42barcelona.com>
2023-08-04 18:52:12 +0200

    libft && minishell.h

---
+++ includes/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecabanas <ecabanas@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/04 18:45:32 by ecabanas          #+#    #+#             */
+/*   Updated: 2023/08/04 18:50:55 by ecabanas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+
+#endif
