a9a760af3fa778a36dbfaf38ec61afdf3f789421
Ester Rosas Cortada <erosas-c@student.42barcelona.com>
2023-09-09 13:02:19 +0200

    cmdsubsplit OK with norminette

--- src/cmdsubsplit.c
+++ src/cmdsubsplit.c
@@ -6,7 +6,7 @@
 /*   By: erosas-c <erosas-c@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 17:42:52 by erosas-c          #+#    #+#             */
-/*   Updated: 2023/09/08 22:18:26 by erosas-c         ###   ########.fr       */
+/*   Updated: 2023/09/09 12:59:24 by erosas-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,53 @@
 #include "../includes/defines.h"
 #include "../lib/libft/libft.h"
 
+static char	*first_spl(char *tr)
+{
+	int		i;
+	char	*sp;
+
+	i = 0;
+	if (tr[0] == '|' || (tr[0] != tr[1]))
+	{
+		sp = malloc (sizeof(char) + 1);
+		if (!sp)
+			return (NULL);
+		sp = ft_substr(tr, 0, 1);
+	}
+	else
+	{
+		sp = malloc (sizeof(char) * 2 + 1);
+		if (!sp)
+			return (NULL);
+		sp = ft_substr(tr, 0, 2);
+	}
+	return (sp);
+}
+
 static char	**trimtosplit(char **trm, char **spl)
 {
-	int i;
+	int	i;
 	int	j;
 
 	i = 0;
 	j = 0;
-	printf("enters trimtosplit\n");
 	while (trm[i])
 	{
-		printf("trm[%i][0]: %c, trm[%i][1]: %c\n", i, trm[i][0], i, trm[i][1]);
 		if (trm[i][0] != '<' && trm[i][0] != '>' && trm[i][0] != '|')
 		{
-			printf("IF\n");
 			spl[j] = malloc (sizeof(char) * (ft_strlen(trm[i]) + 1));
 			if (!spl[j])
 				return (NULL);
-			ft_strlcpy(spl[j], trm[i], ft_strlen(trm[i]) + 1);
-			printf("spl[%i]: %s\n", j, spl[j]);
-		}
-		else if (trm[i][0] != trm[i][1])
-		{
-			printf("ELSE IF\n");
-			spl[j] = malloc (sizeof(char) + 1);
-			if (!spl[j])
-				return (NULL);
-			spl[j++] = ft_substr(trm[i], 0, 1);
-			spl[j] = malloc (sizeof(char) * (ft_strlen(spl[j])));
-			if (!spl[j])
-				return (NULL);
-			spl[j] = ft_substr(trm[i], 1, ft_strlen(spl[j] - 1));
+			ft_strlcpy(spl[j++], trm[i], ft_strlen(trm[i]) + 1);
 		}
 		else
 		{
-			printf("ELSE\n");
-			spl[j] = malloc (sizeof(char) * 2 + 1);
-			if (!spl[j])
-				return (NULL);
-			spl[j++] = ft_substr(trm[i], 0, 2);
-			spl[j] = malloc (sizeof(char) * (ft_strlen(spl[j]) - 1));
-			spl[j] = ft_substr(trm[i], 2, ft_strlen(spl[j] - 2));
+			spl[j++] = first_spl(trm[i]);
+			if (ft_strlen(spl[j - 1]) == 1)
+				spl[j++] = ft_substr(trm[i], 1, ft_strlen(trm[i]));
+			else
+				spl[j++] = ft_substr(trm[i], 2, ft_strlen(trm[i]));
 		}
-		j++;
 		i++;
 	}
 	return (spl);
@@ -66,19 +70,22 @@ static int	count_new_ptrs(char **s)
 {
 	int	num;
 	int	i;
+	int	len;
 
 	num = 0;
 	i = 0;
+	len = 0;
 	while (s[i])
 	{
-		if ((s[i][0] == '<' || s[i][0] == '>' || s[i][0] == '|') && ft_strlen(s[i]) > 1)
+		len = ft_strlen(s[i]);
+		if ((s[i][0] == '<' || s[i][0] == '>' || s[i][0] == '|') && len > 1)
 			num++;
 		i++;
 	}
 	return (num);
 }
 
-char		** cmdsubsplit(char **s, int len)
+char	**cmdsubsplit(char **s, int len)
 {
 	char	**res;
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: erosas-c <erosas-c@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 17:38:52 by erosas-c          #+#    #+#             */
-/*   Updated: 2023/09/08 21:17:05 by erosas-c         ###   ########.fr       */
+/*   Updated: 2023/09/09 13:01:14 by erosas-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,25 @@
 int	main(void)
 {
 	//readl_test();
-	char	cmd[100] = "echo \"hello      there\" how are 'you 'doing? $USER |wc -l >outfile";
+	char	cmd[100] = "echo \"hello      there\" how are 'you 'doing? $USER |wc -l <outfile";
 	char	**trimmed;
 	char	**subsplitted;
-	int	i = 0;
+	int		i = 0;
 
 	trimmed = cmdtrim(cmd);
 	while (trimmed[i])
-	{
+		i++;
+	/*{
 		printf("trimmed[%i]: %s\n", i, trimmed[i]);
 		i++;
-	}
+	}*/
 	subsplitted = cmdsubsplit(trimmed, i);
 	free_all(trimmed, i - 1);
-	
+	i = 0;
+	while (subsplitted[i])
+	{
+		printf("subsplitted[%i]: %s\n", i, subsplitted[i]);
+		i++;
+	}
 	return (0);
 }
