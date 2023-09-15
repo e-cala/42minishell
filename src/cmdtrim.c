c460097d366786f2ebe194ce27fd33494e4ca41f
Ester Rosas Cortada <erosas-c@student.42barcelona.com>
2023-09-15 18:22:33 +0200

    cmdtrim OK with new trim criteria and norminette OK

--- src/cmdtrim.c
+++ src/cmdtrim.c
@@ -6,7 +6,7 @@
 /*   By: erosas-c <erosas-c@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 17:42:52 by erosas-c          #+#    #+#             */
-/*   Updated: 2023/09/14 20:54:17 by erosas-c         ###   ########.fr       */
+/*   Updated: 2023/09/15 18:20:43 by erosas-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,9 @@
 #include "../includes/defines.h"
 #include "../lib/libft/libft.h"
 
-/*static int	ft_upd_je(char *s, int j)
-{
-	if ((s[j] == '>' && s[j + 1] == '>') || (s[j] == '<' && [j + 1] == '<'))
-		j = j + 2;
-	else
-		j++;
-	while (s[j] && s[j] != '<' && s[j] != '>' && s[j] != '|' && s[j] != KSPACE)
-		j++;
-	else
-	{
-		while (s[j] && s[j] != KSPACE && s[j] != )
-			j++;
-	return (j);
-}*/
-
 static int	ft_upd_j(char *s, int j, int i)
 {
-	if (i == 1)
+	if (i == 0)
 	{
 		while (s[j] && s[j] != KSPACE)
 			j++;
@@ -55,106 +40,64 @@ static int	ft_upd_j(char *s, int j, int i)
 	return (j);
 }
 
-/*static int	ft_ptr_len(char const *p)
+static int	upd_jcount(int j, char *s)
 {
-	int	i;
-
-	i = 0;
-	if (p[i] == DQUOTE)
+	while (s[j])
 	{
-		i++;
-		while (p[i] != DQUOTE)
-			i++;
-		i++;
-		while (p[i] && p[i] != KSPACE)
-			i++;
+		while (s[j] && s[j] != KSPACE && s[j] != SQUOTE && s[j] != DQUOTE)
+			j++;
+		if (s[j] && s[j] == KSPACE)
+			break ;
+		else if (s[j])
+		{
+			j = ft_upd_j(s, j, 1);
+			if (s[j] && s[j] == KSPACE)
+				break ;
+			j++;
+		}
 	}
-	else if (p[i] == SQUOTE)
-	{
-		i++;
-		while (p[i] != SQUOTE)
-			i++;
-		i++;
-		while (p[i] && p[i] != KSPACE)
-			i++;
-	}
-	else
-		while (p[i] != KSPACE && p[i])
-			i++;
-	return (i);
-}*/
+	return (j);
+}
 
 static int	ft_ptr_len(char *p)
 {
 	int	i;
-	int in;
 
 	i = 0;
-	in = 0;
 	while (p[i])
 	{
-		if (p[i] && (p[i] == DQUOTE || p[i] == SQUOTE))
-			i = ft_upd_j(p, i, 0);
-
-		else
+		while (p[i] && p[i] != KSPACE && p[i] != SQUOTE && p[i] != DQUOTE)
+			i++;
+		if (p[i] && p[i] == KSPACE)
+			break ;
+		else if (p[i])
 		{
 			i = ft_upd_j(p, i, 1);
-			break ;
+			if (p[i] && p[i] == KSPACE)
+				break ;
+			i++;
 		}
 	}
-	/*if (p[i] == DQUOTE)
-	{
-		i++;
-		while (p[i] != DQUOTE)
-			i++;
-		i++;
-		while (p[i] && p[i] != KSPACE)
-			i++;
-	}
-	else if (p[i] == SQUOTE)
-	{
-		i++;
-		while (p[i] != SQUOTE)
-			i++;
-		i++;
-		while (p[i] && p[i] != KSPACE)
-			i++;
-	}
-	else
-		while (p[i] != KSPACE && p[i])
-			i++;*/
 	return (i);
 }
-
 
 int	ft_str_count(char *s)
 {
 	int	str_count;
 	int	j;
-//	int	in;
 
-//	in = 0;
 	str_count = 0;
 	j = 0;
 	if (*s == '\0')
 		return (0);
 	while (s[j])
 	{
-		/*if (s[j] == KSPACE)
-		{*/
 		while (s[j] && s[j] == KSPACE)
 			j++;
-		//}
 		if (s[j])
 			str_count++;
-		while (s[j] && s[j] != KSPACE && s[j] != DQUOTE && s[j] != SQUOTE)
-			j++;
-		if (s[j] && (s[j] == DQUOTE || s[j] == SQUOTE))
-			j = ft_upd_j(s, j, 1);
-		else
-			j = ft_upd_j(s, j, 0);
+		j = upd_jcount(j, s);
 	}
-//	printf("str_count: %i\n", str_count);
 	return (str_count);
 }
 
